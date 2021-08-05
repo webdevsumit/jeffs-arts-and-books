fbf4da7ac6af5fbf207a95b5b248337f05140d81
Sumit Dhakad <sumitdhakad2232@gmail.com>
2021-08-05 10:50:48 +0530

    more anim in lower sec

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ window.addEventListener("scroll",function () {
 				var comP2 = document.getElementById("about-company-p2");
 				var comP3 = document.getElementById("about-company-p3");
 				var comP4 = document.getElementById("about-company-p4");
+				var comP5 = document.getElementById("about-company-p5");
 				
 				if (window.pageYOffset>=200) {
 								comP2.style.opacity = 1;
@@ -25,5 +26,75 @@ window.addEventListener("scroll",function () {
 				}else {
 								comP4.style.opacity = 0;
 								comP4.style.transform = "translateY(30px)";
+				};
+				if (window.pageYOffset>=440) {
+								comP5.style.opacity = 1;
+								comP5.style.transform = "translateY(0px)";
+				}else {
+								comP5.style.opacity = 0;
+								comP5.style.transform = "translateY(30px)";
+				};
+				
+				var authH = document.getElementById("author-h");
+				var authP1 = document.getElementById("author-p1");
+				var authP2 = document.getElementById("author-p2");
+				var authP3 = document.getElementById("author-p3");
+				var authP4 = document.getElementById("author-p4");
+				
+				if (window.pageYOffset>=700 && window.innerWidth<500) {
+								authH.style.opacity = 1;
+								authH.style.transform = "translateX(0px)";
+				}else if (window.pageYOffset>=480 && window.innerWidth>500) {
+								authH.style.opacity = 1;
+								authH.style.transform = "translateX(0px)";
 				}
+				else {
+								authH.style.opacity = 0;
+								authH.style.transform = "translateX(-30px)";
+				};
+				if (window.pageYOffset>=800) {
+								authP1.style.opacity = 1;
+								authP1.style.transform = "translateX(0px)";
+				}else if (window.pageYOffset>=580 && window.innerWidth>500) {
+								authP1.style.opacity = 1;
+								authP1.style.transform = "translateX(0px)";
+				}
+				else {
+								authP1.style.opacity = 0;
+								authP1.style.transform = "translateX(-30px)";
+				};
+				if (window.pageYOffset>=900) {
+								authP2.style.opacity = 1;
+								authP2.style.transform = "translateX(0px)";
+				}else if (window.pageYOffset>=700 && window.innerWidth>500) {
+								authP2.style.opacity = 1;
+								authP2.style.transform = "translateX(0px)";
+				}
+				else {
+								authP2.style.opacity = 0;
+								authP2.style.transform = "translateX(-30px)";
+				};
+				if (window.pageYOffset>=1000) {
+								authP3.style.opacity = 1;
+								authP3.style.transform = "translateX(0px)";
+				}else if (window.pageYOffset>=800 && window.innerWidth>500) {
+								authP3.style.opacity = 1;
+								authP3.style.transform = "translateX(0px)";
+				}
+				else {
+								authP3.style.opacity = 0;
+								authP3.style.transform = "translateX(-30px)";
+				};
+				if (window.pageYOffset>=1050) {
+								authP4.style.opacity = 1;
+								authP4.style.transform = "translateX(0px)";
+				}else if (window.pageYOffset>=850 && window.innerWidth>500) {
+								authP4.style.opacity = 1;
+								authP4.style.transform = "translateX(0px)";
+				}
+				else {
+								authP4.style.opacity = 0;
+								authP4.style.transform = "translateX(-30px)";
+				};
+				
 });
